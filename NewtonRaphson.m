3b59e29a0bdc8d6747feca7dccc200e901d65af4
RakibulRanak <43205114+RakibulRanak@users.noreply.github.com>
2019-10-29 03:51:59 +0600

    Octave

---
+++ NewtonRaphson.m
@@ -0,0 +1,15 @@
+pkg load symbolic;
+syms x;
+fun=input('Enter the function as a variable of x: ');
+f=inline(fun);
+z=diff(f(x));
+f1=function_handle(z);
+x0=input('Enter the starting value of x: ');
+for i=2:100
+  x1=x0-(f(x0)/f1(x0));
+  x1
+  if(x1==x0)
+  break;
+  endif;
+  x0=x1;
+endfor
